aa18826a4b82868f92f7a86527450d516bc75c98
Kauan <kauancdmoraes1@gmail.com>
2026-06-11 13:12:14 -0300

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+g = 9.81
+
+m = 1.5  # masse drone [kg]
+
+J = np.diag([
+    0.025,  # Jx
+    0.025,  # Jy
+    0.045   # Jz
+])
+
+J_inv = np.linalg.inv(J)
---
+++ rotation.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def rotation_matrix(phi, theta, psi):
+    """
+    Matrice de rotation body -> inertiel
+    Angles d'Euler ZYX : yaw psi, pitch theta, roll phi
+    """
+
+    cphi, sphi = np.cos(phi), np.sin(phi)
+    ctheta, stheta = np.cos(theta), np.sin(theta)
+    cpsi, spsi = np.cos(psi), np.sin(psi)
+
+    R = np.array([
+        [cpsi*ctheta, cpsi*stheta*sphi - spsi*cphi, cpsi*stheta*cphi + spsi*sphi],
+        [spsi*ctheta, spsi*stheta*sphi + cpsi*cphi, spsi*stheta*cphi - cpsi*sphi],
+        [-stheta,      ctheta*sphi,                         ctheta*cphi]
+    ])
+
+    return R
